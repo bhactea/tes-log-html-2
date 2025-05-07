7c3727e350b5a09a426381b0432a99756f04edb2
bhactea <45590138+bhactea@users.noreply.github.com>
2025-05-07 11:41:01 +0700

    Create WritableArray.java

---
+++ app/src/main/java/com/facebook/react/bridge/WritableArray.java
@@ -0,0 +1,6 @@
+// app/src/main/java/com/facebook/react/bridge/WritableArray.java
+package com.facebook.react.bridge;
+public interface WritableArray {
+  void pushString(String value);
+  void pushInt(int value);
+}
