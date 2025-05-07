cbd9a51fc26589fb792009ca119af2d56c611c65
bhactea <45590138+bhactea@users.noreply.github.com>
2025-05-07 11:35:14 +0700

    Create ReadableArray.java

---
+++ app/src/main/java/com/facebook/react/bridge/ReadableArray.java
@@ -0,0 +1,6 @@
+// app/src/main/java/com/facebook/react/bridge/ReadableArray.java
+package com.facebook.react.bridge;
+public interface ReadableArray {
+  int size();
+  // deklarasi lain kalau perlu …
+}
