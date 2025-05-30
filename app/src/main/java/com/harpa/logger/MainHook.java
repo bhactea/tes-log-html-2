23521b22e164cce3a909dfd1761fd5bdda39c537
Bhakti Setyawan <emailkamu@example.com>
2025-05-30 14:55:53 +0700

    ✨ Hook log semua package ke harpa_log.html + fix manifest build

--- app/src/main/java/com/harpa/logger/MainHook.java
+++ app/src/main/java/com/harpa/logger/MainHook.java
@@ -1,36 +1,32 @@
 package com.harpa.logger;
 
 import android.os.Environment;
-import android.util.Log;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
+import java.util.Locale;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import de.robv.android.xposed.XposedBridge;
 
 public class MainHook implements IXposedHookLoadPackage {
-    private static final String TAG = "HarpaLogger";
+
+    private void logToHtml(String packageName) {
+        try {
+            File dir = new File(Environment.getExternalStorageDirectory(), "harpa_log.html");
+            FileWriter fw = new FileWriter(dir, true);
+            String time = new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(new Date());
+            String html = "<p>[" + time + "] Loaded: <b>" + packageName + "</b></p>\n";
+            fw.write(html);
+            fw.close();
+        } catch (Exception e) {
+            XposedBridge.log("Logger error: " + e.getMessage());
+        }
+    }
 
     @Override
-    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
-        try {
-            String time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-            String line = "<p>[" + time + "] Loaded: <b>" + lpparam.packageName + "</b></p>\\n";
-
-            File logFile = new File(Environment.getExternalStorageDirectory(), "harpa-log.html");
-            if (!logFile.exists()) {
-                FileWriter initWriter = new FileWriter(logFile);
-                initWriter.write("<html><body><h2>Harpa Logger Log</h2>\\n");
-                initWriter.close();
-            }
-            FileWriter writer = new FileWriter(logFile, true);
-            writer.write(line);
-            writer.close();
-        } catch (Throwable t) {
-            Log.e(TAG, "Error logging package", t);
-        }
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) {
+        logToHtml(lpparam.packageName);
     }
 }
