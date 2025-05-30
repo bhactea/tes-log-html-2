67390c665d6ff9c5242a6c25bcce3b0d41bd860c
Bhakti Setyawan <emailkamu@example.com>
2025-05-30 14:22:17 +0700

    🔥 Setup lengkap untuk LSPosed Logger Harpa

---
+++ app/src/main/java/com/harpa/logger/MainHook.java
@@ -0,0 +1,12 @@
+package com.harpa.logger;
+
+import android.util.Log;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class MainHook implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        Log.i("HarpaLogger", "📦 Loaded app: " + lpparam.packageName);
+    }
+}
