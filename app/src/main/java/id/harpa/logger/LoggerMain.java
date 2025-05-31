5ff083869b4c47f91a37a76ffe4ef4bfd1a3d382
bhactea <bhakti@example.com>
2025-05-31 15:03:22 +0700

    ✅ Tambah LoggerMain dan konfigurasi LSPosed

---
+++ app/src/main/java/id/harpa/logger/LoggerMain.java
@@ -0,0 +1,12 @@
+package id.harpa.logger;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import de.robv.android.xposed.XposedBridge;
+
+public class LoggerMain implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("✅ Harpa Logger aktif di: " + lpparam.packageName);
+    }
+}
