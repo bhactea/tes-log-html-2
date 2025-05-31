212853779bba9d8613bd08a5345426d4a1e82e87
bhactea <bhakti@example.com>
2025-05-31 21:55:38 +0700

    ✅ Fix: build APK agar terdeteksi LSPosed & bisa install manual

--- app/src/main/java/id/harpa/logger/LoggerMain.java
+++ app/src/main/java/id/harpa/logger/LoggerMain.java
@@ -5,8 +5,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import de.robv.android.xposed.XposedBridge;
 
 public class LoggerMain implements IXposedHookLoadPackage {
-    @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        XposedBridge.log("✅ Harpa Logger aktif di: " + lpparam.packageName);
+        XposedBridge.log("HarpaLogger active in: " + lpparam.packageName);
     }
 }
