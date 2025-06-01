4f9807a23292c8b4bbbe120356dfd7c9fdc4162b
bhactea <bhakti@example.com>
2025-06-01 15:00:08 +0700

    ✨ Add MainHook.kt to log onCreate of Harpa Mobile

---
+++ app/src/main/java/id/harpa/logger/MainHook.kt
@@ -0,0 +1,24 @@
+package id.harpa.logger
+
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+class MainHook : IXposedHookLoadPackage {
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        if (lpparam.packageName == "com.harpamobilehr") {
+            XposedBridge.log("HarpaLogger aktif di: ${lpparam.packageName}")
+            XposedHelpers.findAndHookMethod(
+                "android.app.Application",
+                lpparam.classLoader,
+                "onCreate",
+                object : XC_MethodHook() {
+                    override fun afterHookedMethod(param: MethodHookParam?) {
+                        LogXposed.log("HOOK", "onCreate Harpa dijalankan!")
+                    }
+                })
+        }
+    }
+}
