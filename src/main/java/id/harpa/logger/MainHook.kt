7ad3765de5d2c4c40b8638df67bfd852d5d6e2fc
bhactea <bhakti@example.com>
2025-06-02 05:37:27 +0700

    📦 Struktur modular: pindah LSPosed ke 'app/', dummy installer di 'dummy/'

---
+++ src/main/java/id/harpa/logger/MainHook.kt
@@ -0,0 +1,13 @@
+package id.harpa.logger
+
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+class MainHook : IXposedHookLoadPackage {
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        if (lpparam.packageName != "com.harpamobilehr") return
+
+        XposedBridge.log("🎯 HarpaLogger Loaded on: " + lpparam.packageName)
+    }
+}
